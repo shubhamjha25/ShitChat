2c694254badc316fb3d8d238593a8fb2385c23f7
Shubham Jha <shubham.jha2001m@gmail.com>
2021-08-29 21:51:13 +0530

    Login Screen

--- App.js
+++ App.js
@@ -2,13 +2,15 @@ import React from 'react';
 import { StyleSheet } from 'react-native';
 import {createStackNavigator} from '@react-navigation/stack'
 import {NavigationContainer} from '@react-navigation/native';
+import LoginScreen from './screens/Login';
 
 const Stack = createStackNavigator();
+
 const App = () => {
   return (
     <NavigationContainer>
       <Stack.Navigator >
-        
+        <Stack.Screen name="Login" component={LoginScreen} />
       </Stack.Navigator>
     </NavigationContainer>
   );
