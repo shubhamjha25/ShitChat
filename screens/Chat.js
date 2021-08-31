3a877a392ab2cd8aac9f7c55e893e002f85568d8
Shubham Jha <shubham.jha2001m@gmail.com>
2021-08-31 12:38:19 +0530

    Retrieving Old Messages from `firestore`

--- screens/Chat.js
+++ screens/Chat.js
@@ -55,6 +55,18 @@ const Chat = ({ navigation }) => {
         ])
     }, []);
 
+    useLayoutEffect(
+        () => {
+            const unsubscribe = db.collection('chats').orderBy('createdAt', 'desc').onSnapshot(snapshot => setMessages(
+                snapshot.docs.map(doc => ({
+                    _id: doc.data()._id,
+                    createdAt: doc.data().createdAt.toDate(),
+                    text: doc.data().text,
+                    user: doc.data().user,
+                }))
+            )
+    )});
+
     const onSend = useCallback((messages = []) => {
         setMessages(previousMessages => GiftedChat.append(previousMessages, messages));
         const { _id, createdAt, text, user,} = messages[0];
